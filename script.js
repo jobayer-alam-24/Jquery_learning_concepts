dc60bc7dd03f7bfb785441273f3deb7ce3fd4622
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-11-21 18:26:32 +0600

    learning...

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-$(function () {
-    $("h5").bind('click', function (e) {
-        console.log(e);
+//day 1
+/* Document Ready function: write all the  jquery code inside this function 
+    !1. $(selector).action()
+*/
+$(function () //Document Ready Function
+{
+    $('.btn').click((e) => 
+    {
+        console.log(e.target);
     });
 });
+ //!2. Types of Selectors : element, class, id, types, attributes, values of attributes... more: *, this (current html element), [attributeName], tr:even, tr:odd
+ // selector starts with - $('selector') 
+$(function () {
+    $('.btn').click(() =>
+    {
+        $('tr:odd').addClass("color")
+        $('tr:even').addClass('color2');
+    })
+    $('div').bind('click', () => {
+        $('div').hide();
+    })
+    $('.btn').bind("mouseenter", function ()
+        {
+            $('div').show();
+            $('*').addClass('allColor');
+        }
+    )
+    $('.btn').bind('mouseleave', () => {
+        $("*").removeClass('allColor');
+        $('#myId').hide();
+    })
+    $('[name="username"]').bind('change', (e) => 
+    {
+        var value = "Email: " + e.target.value;
+        $('#showName').html(value);
+    })
+})
