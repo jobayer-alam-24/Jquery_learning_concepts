5a712c19f006258d7f99bfc777007bc0ad416e17
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-11-22 15:16:00 +0600

    learning

--- script.js
+++ script.js
@@ -2,37 +2,65 @@
 /* Document Ready function: write all the  jquery code inside this function 
     !1. $(selector).action()
 */
-$(function () //Document Ready Function
-{
-    $('.btn').click((e) => 
-    {
-        console.log(e.target);
-    });
-});
+// $(function () //Document Ready Function
+// {
+//     $('.btn').click((e) => 
+//     {
+//         console.log(e.target);
+//     });
+// });
  //!2. Types of Selectors : element, class, id, types, attributes, values of attributes... more: *, this (current html element), [attributeName], tr:even, tr:odd
  // selector starts with - $('selector') 
-$(function () {
-    $('.btn').click(() =>
-    {
-        $('tr:odd').addClass("color")
-        $('tr:even').addClass('color2');
-    })
-    $('div').bind('click', () => {
-        $('div').hide();
-    })
-    $('.btn').bind("mouseenter", function ()
+// $(function () {
+//     $('.btn').click(() =>
+//     {
+//         $('tr:odd').addClass("color")
+//         $('tr:even').addClass('color2');
+//     })
+//     $('div').bind('click', () => {
+//         $('div').hide();
+//     })
+//     $('.btn').bind("mouseenter", function ()
+//         {
+//             $('div').show();
+//             $('*').addClass('allColor');
+//         }
+//     )
+//     $('.btn').bind('mouseleave', () => {
+//         $("*").removeClass('allColor');
+//         $('#myId').hide();
+//     })
+//     $('[name="username"]').bind('change', (e) => 
+//     {
+//         var value = "Email: " + e.target.value;
+//         $('#showName').html(value);
+//     })
+// })
+//Day 2
+$(function ()
+{
+    $('.box').on(
         {
-            $('div').show();
-            $('*').addClass('allColor');
+            click : (e) => console.log(e.target),
+            dblclick : function ()
+            {
+                $(this).css('background-color', 'green');
+            },
+            mouseenter : () => console.log("Mouse enter.."),
+            mouseleave : () => console.log("Mouse leave..."),
+            mouseup : () => console.log("mouseup"),
+            mousedown : () => console.log("mouse down"),
+        }   
+    )
+    $('.btn').on(
+        {
+            keyup : () => console.log("Key up"),
+            keydown : () => console.log("Key down"),
+            keypress : () => console.log("key press"),
         }
     )
-    $('.btn').bind('mouseleave', () => {
-        $("*").removeClass('allColor');
-        $('#myId').hide();
-    })
-    $('[name="username"]').bind('change', (e) => 
+    $(window).bind('resize', (e) => 
     {
-        var value = "Email: " + e.target.value;
-        $('#showName').html(value);
+        console.log(e.target.innerWidth);
     })
 })
