01ad3a1f3c1e5db73fa3d62f33cb828ffb142f5f
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-11-23 15:34:10 +0600

    learning

--- script.js
+++ script.js
@@ -37,30 +37,74 @@
 //     })
 // })
 //Day 2
-$(function ()
+// $(function ()
+// {
+//     $('.box').on(
+//         {
+//             click : (e) => console.log(e.target),
+//             dblclick : function ()
+//             {
+//                 $(this).css('background-color', 'green');
+//             },
+//             mouseenter : () => console.log("Mouse enter.."),
+//             mouseleave : () => console.log("Mouse leave..."),
+//             mouseup : () => console.log("mouseup"),
+//             mousedown : () => console.log("mouse down"),
+//         }   
+//     )
+//     $('.btn').on(
+//         {
+//             keyup : () => console.log("Key up"),
+//             keydown : () => console.log("Key down"),
+//             keypress : () => console.log("key press"),
+//         }
+//     )
+//     $(window).bind('resize', (e) => 
+//     {
+//         console.log(e.target.innerWidth);
+//     })
+// })
+// Day 3 
+/*
+    *Methods: show(), hide(), toggle(), fadeIn(), fadeOut(), fadeToggle(), fadeTo()
+    * (speed, opcity(ony for fadeTo), callback)
+    * speed = "slow", "fast", miliseconds
+    * opacity = 0 - 1
+    * callback = is a function that is passed as an argument to another function
+*/
+$(function() 
 {
-    $('.box').on(
-        {
-            click : (e) => console.log(e.target),
-            dblclick : function ()
-            {
-                $(this).css('background-color', 'green');
-            },
-            mouseenter : () => console.log("Mouse enter.."),
-            mouseleave : () => console.log("Mouse leave..."),
-            mouseup : () => console.log("mouseup"),
-            mousedown : () => console.log("mouse down"),
-        }   
-    )
-    $('.btn').on(
-        {
-            keyup : () => console.log("Key up"),
-            keydown : () => console.log("Key down"),
-            keypress : () => console.log("key press"),
-        }
-    )
-    $(window).bind('resize', (e) => 
+    $('#show').bind('click', () => 
     {
-        console.log(e.target.innerWidth);
+        $('#bird').show('slow', alertName("Show")).fadeIn("slow");
+    })
+    $('#hide').bind('click', () => 
+    {
+        $('#bird').hide(3000).fadeOut();
+    })
+    $('#toggle').bind('click', () => 
+    {
+        $('#bird2').toggle("slow")
+    })
+    $('.heading').click(() =>  
+    {
+        // $(".heading-body").fadeToggle("fast");
+        $('.heading-body').fadeToggle();
+    })
+    $('.btn').bind('click', () => {
+        $('.box').fadeTo("slow", .5, () => console.log("Fade to Done")).hide("slow");
+    });
+})
+function alertName(name)
+{
+    alert("image " + name);
+}
+$(function () 
+{
+    $('.olive-image').hide();
+    $('.olive-pic').hide();
+    $('.bars').bind('click', () => {
+        $('.olive-image').fadeToggle(2000); 
+        $('.olive-pic').show();
     })
 })
